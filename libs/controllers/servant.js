24135b5fd0cf68f291f5f707f6e560078a089bb3
K <khayusaki@gmail.com>
2016-09-20 13:03:48 +0800

    Servants

---
+++ libs/controllers/servant.js
@@ -0,0 +1,8 @@
+Var Servant = require('../models/servant');
+var ServantList = require('../models/servantlist');
+
+// Get servants by ServerList
+exports.getSerantsByList = function (req, res) {}
+
+// Add new Servant
+exports.addServant = function (req, res) {}
